1fbef9f8f3ed4a952afae1d147e561b2d5c9872a
Filipe Arruda <filipehenriquester@gmail.com>
2024-04-01 20:26:48 -0300

    initial commit

---
+++ POCCSharpSonar/Somar.cs
@@ -0,0 +1,9 @@
+﻿namespace POCCSharpSonar;
+
+public class Somar
+{
+    public int Soma(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+}
---
+++ POCCSharpSonar/SomarTest.cs
@@ -0,0 +1,12 @@
+﻿namespace POCCSharpSonar;
+
+[TestFixture]
+public class SomarTest
+{
+    [Test]
+    public void Test1()
+    {
+        Somar somar = new Somar();
+        Assert.That(somar.Soma(1, 1), Is.EqualTo(2));
+    }
+}
