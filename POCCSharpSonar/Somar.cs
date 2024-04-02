f36bfc0604d6478917aff14cc43c69f774fabefe
Filipe Arruda <filipehenriquester@gmail.com>
2024-04-02 01:14:51 -0300

    Novos métodos

--- POCCSharpSonar/Somar.cs
+++ POCCSharpSonar/Somar.cs
@@ -6,4 +6,24 @@ public class Somar
     {
         return n1 + n2;
     }
+    
+    public double Soma(double n1, double n2)
+    {
+        return n1 + n2;
+    }
+    
+    public float Soma(float n1, float n2)
+    {
+        return n1 + n2;
+    }
+    
+    public uint Soma(uint n1, uint n2)
+    {
+        return n1 + n2;
+    }
+    
+    public nint Soma(nint n1, nint n2)
+    {
+        return n1 + n2;
+    }
 }
